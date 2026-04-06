f4b871141ee7aca6471bb4dd54b9317dab512a0b
tristandotdev <tristandepoosingh001@gmail.com>
2026-04-06 16:01:49 -0400

    Worked on heroes page

---
+++ public/heroes.js
@@ -0,0 +1,35 @@
+import { DEADLOCK_ASSETS_API_URL } from "./constants.js";
+let heroes = [];
+
+async function fetchHeroData() {
+  let response = await fetch(`${DEADLOCK_ASSETS_API_URL}/v2/heroes`);
+  heroes = await response.json();
+  
+  displayHeroes(heroes); 
+}
+
+function displayHeroes(heroes) {
+  console.log(heroes);
+
+  const insertCards = document.querySelector(".cards");
+  let html = "";
+
+  //add in paragraph if you'd like <p style="color: rgba(255, 255, 255, 0.7); margin-top: 10px">${h.name}</p>
+
+  heroes.forEach(h => {
+    if(!h.in_development && h.name != "Kali") {
+      html += `
+      <div>
+        <img src="${h.images.top_bar_vertical_image}" 
+        style="border-color: rgb(255, 255, 255);
+        border-width: 1px;
+        background-image: url("${h.images.background_image_webp}");">
+      </div>
+      `
+    }
+  });
+
+  insertCards.innerHTML = html;
+}
+
+fetchHeroData();
