ac46a3af70b15665f8d14931f6cd007e536e9e17
tristandotdev <tristandepoosingh001@gmail.com>
2026-04-07 12:31:08 -0400

    added basic modal implementation

--- public/scripts/heroes.js
+++ public/scripts/heroes.js
@@ -4,8 +4,41 @@ let heroes = [];
 async function fetchHeroData() {
   let response = await fetch(`${DEADLOCK_ASSETS_API_URL}/v2/heroes`);
   heroes = await response.json();
-  
   displayHeroes(heroes, ""); 
+}
+
+window.showMore = function(index) {
+  const modalContent = document.querySelector(".modal-content");
+  const modal = document.getElementById("modal");
+  modal.style.display = "block";
+
+  if(heroes[index].description.role == undefined || heroes[index].description.playstyle == undefined) {
+    modalContent.innerHTML = `
+    <h1 style="font-size: 3rem;">${heroes[index].name}</h1>
+      <div style="display: flex; flex-direction: row; justify-items: flex-start; align-items: flex-start; margin: 0px; margin-top: 15px;">
+          <p style="padding-right: 5%; font-size: 1.2rem;">${heroes[index].description.lore}</p>
+          <img src="${heroes[index].images.top_bar_vertical_image}" style="height:100%; margin-right: 20px;">
+      </div>
+    `;
+  }
+  
+  else {
+  modalContent.innerHTML = `
+   <h1 style="font-size: 3rem;">${heroes[index].name}</h1>
+   <p>${heroes[index].description.role}</p>
+   <p>${heroes[index].description.playstyle}</p>
+    <div style="display: flex; flex-direction: row; justify-items: flex-start; align-items: flex-start; margin: 0px; margin-top: 15px;">
+        <p style="padding-right: 5%; font-size: 1.2rem;">${heroes[index].description.lore}</p>
+        <img src="${heroes[index].images.top_bar_vertical_image}" style="height:100%; margin-right: 20px;">
+    </div>
+  `;
+  }
+}
+
+window.onclick = function(event) {
+  if(event.target == modal) {
+    modal.style.display = "none";
+  }
 }
 
 function displayHeroes(heroes, filter) {
@@ -14,13 +47,13 @@ function displayHeroes(heroes, filter) {
 
   //add in paragraph if you'd like <p style="color: rgba(255, 255, 255, 0.7); margin-top: 10px">${h.name}</p>
 
-  heroes.forEach(h => {
-    h.name = h.name.toLowerCase();
-    filter = filter.toLowerCase();
+  heroes.forEach((h, index) => {
+    const name = h.name.toLowerCase();
+    const search = filter.toLowerCase();
 
-    if(!h.in_development && h.name != "kali" && h.name.includes(filter)) {
+  if (!h.in_development && name != "kali" && name.includes(search) && h.player_selectable) {
       html += `
-      <div>
+      <div onclick="showMore(${index})">
         <img src="${h.images.top_bar_vertical_image}" 
         style="border-color: rgb(255, 255, 255);
         border-width: 1px;
@@ -36,7 +69,7 @@ function displayHeroes(heroes, filter) {
 const inputBox = document.getElementById("heroSearch");
 
 inputBox.addEventListener("input", () => {
-  console.log(inputBox.value);
+  //console.log(inputBox.value);
   displayHeroes(heroes, inputBox.value);
 });
 
